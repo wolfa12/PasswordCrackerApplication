63d8fa15de680053aa40422beb48641e8c97c3c2
Courtney <campbell.1807@osu.edu>
2019-03-03 13:31:06 -0500

    start password loader file

---
+++ parsePasswords.py
@@ -0,0 +1,14 @@
+# written in Python 3
+# used https://www.w3schools.com/python/default.asp
+
+# open text file of the most common passwords
+file = open("passwords.txt", "r")
+
+# create list
+wordlist = []
+
+# loop through the file and add them into a list
+for line in file:
+    wordlist.append(line)
+
+    
