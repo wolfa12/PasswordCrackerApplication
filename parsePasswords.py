ec7d42eb3acdaa287031b0aec184aaac5c0db50b
Courtney <campbell.1807@osu.edu>
2019-03-12 15:52:34 -0400

    start add, delete, search methods

--- parsePasswords.py
+++ parsePasswords.py
@@ -10,3 +10,22 @@ wordlist = []
 # loop through the file and add them into a list
 for line in file:
     wordlist.append(line)
+
+
+
+# ask user if they have any other words to add (don't add a duplicate word if it's there already)
+def add_word(userword):
+    
+# ask user if they have words to delete
+def delete_word(userword):
+
+
+# ask user if they're asking for a specific word in the list (i.e. search for a word)
+#prompt user
+
+def search_word(userword):
+    if userword in wordlist:
+        print("This word is in the existing list")
+    else:
+        print("This word doesn't exist in the list. Would you like to add it? Type yes or no")
+        # add more to this later
