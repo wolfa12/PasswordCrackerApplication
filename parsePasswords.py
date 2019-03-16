a4ef2bbafd8d8e73ee5356142c929db51c0d75bf
Courtney <campbell.1807@osu.edu>
2019-03-16 12:44:13 -0400

    finish up dictionary methods

--- parsePasswords.py
+++ parsePasswords.py
@@ -1,31 +1,41 @@
 # written in Python 3
 # used https://www.w3schools.com/python/default.asp
 
-# open text file of the most common passwords
-file = open("passwords.txt", "r")
-
-# create list
-wordlist = []
-
-# loop through the file and add them into a list
-for line in file:
-    wordlist.append(line)
-
-
+# add in the pre-defined dictionary
+def add_prev:
+    # open text file of the most common passwords
+    file = open("passwords.txt", "r")
+    # create list
+    wordlist = []
+    # loop through the file and add them into a list
+    for line in file:
+        wordlist.append(line)
 
 # ask user if they have any other words to add (don't add a duplicate word if it's there already)
 def add_word(userword):
-    
+    # check if the word is already in the list, if not, add it
+    if userword not in wordlist:
+        wordlist.append(userword)
+
 # ask user if they have words to delete
 def delete_word(userword):
-
+    if userword in wordlist:
+        wordlist.remove(userword)
+    else:
+        print("That word doesn't exist in the dictionary and cannot be deleted.")
 
 # ask user if they're asking for a specific word in the list (i.e. search for a word)
-#prompt user
-
 def search_word(userword):
     if userword in wordlist:
-        print("This word is in the existing list")
+        print("This word is in the existing list.")
     else:
-        print("This word doesn't exist in the list. Would you like to add it? Type yes or no")
+        prompt = raw_input("This word doesn't exist in the list. Would you like to add it? Type yes or no")
         # add more to this later
+        if prompt == "yes":
+            word = raw_input("What word would you like to add?")
+            add_word(word)
+        elif prompt == "no":
+            # move on
+            print("Please continue.")
+        else:
+            print("Please type yes or no.")
