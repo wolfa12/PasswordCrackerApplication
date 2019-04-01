2c931d553bb588f7474fbaf191b167f39c3bb3c5
Courtney <campbell.1807@osu.edu>
2019-03-31 23:51:02 -0400

    start facebook scraper

---
+++ facebook_scraper.py
@@ -0,0 +1,11 @@
+# from urllib2 import urlopen
+# urlopen(https://www.facebook.com/tara.bite.33)
+import requests
+from bs4 import BeautifulSoup
+
+facebookpage_url = 'https://www.facebook.com/tara.bite.33'
+response = requests.get(facebookpage_url)
+html = response.content
+
+soup = BeautifulSoup(html)
+print(soup.prettify())
