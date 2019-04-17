5c2f24a00fe7112378bab09db5ac773e13cb1473
Courtney <campbell.1807@osu.edu>
2019-04-16 22:30:45 -0400

    Remove random comments/files

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -10,7 +10,7 @@ reddit = praw.Reddit(client_id='qIv6ZQYkFNpvAQ', \
 
 # get URL for each comment
 # allow user to click to view more
-# print all results to the page 
+# print all results to the page
 
 # https://www.reddit.com/user/thisisbillgates
 print("Bill Gates's top comments of all time")
@@ -21,33 +21,6 @@ for comment in reddit.redditor('thisisbillgates').comments.new(limit=5):
 print("Bill Gates's top 10 submissions")
 for topsub in reddit.redditor('thisisbillgates').submissions.top('all', limit=5):
     print(topsub.title)
-
-# controversial
-# for cont in reddit.redditor('thisisbillgates').controversial('all', limit=5):
-#     print(cont.title)
-
-
-# # old from facebook scraper
-# import requests
-# from bs4 import BeautifulSoup
-#
-# facebookpage_url = 'https://www.facebook.com/tara.bite.33'
-# # need to do error handling here
-# response = requests.get(facebookpage_url)
-# html = response.content
-#
-# soup = BeautifulSoup(html, features="lxml")
-# #print(soup.prettify())
-# # do more here of course
-#
-# # get the titles
-# # <ul class="uiList fbProfileEditExperiences _4kg _4ks"> - contains major and school
-#
-# # write to a file (for testing)
-# file = open("testfile.txt", "w")
-# file.write(soup.prettify())
-# file.close()
-
 
 
 # personal use script: PZ4WFNV5lz-rYQ
